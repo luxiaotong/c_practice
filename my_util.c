e7557d80bc7401b135195f0464113745dd83d3d9
luxiaotong <luxiaotong713@sina.com>
2015-03-11 17:01:27 +0800

    Add timeval_sub function

--- my_util.c
+++ my_util.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "my_util.h"
 
 void parray(int *array, int len)
@@ -9,5 +8,17 @@ void parray(int *array, int len)
     }
     printf("\n");
     
+    return;
+}
+
+void timeval_sub(struct timeval *out, struct timeval *in)
+{       
+    if( (out->tv_usec -= in->tv_usec) < 0 )
+    {   
+        --out->tv_sec;
+        out->tv_usec += 1000000;
+    }
+    out->tv_sec -= in->tv_sec;
+
     return;
 }
--- my_util.h
+++ my_util.h
@@ -1,6 +1,11 @@
 #ifndef MY_UTIL_H
 #define MY_UTIL_H 1
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 void parray(int *array, int len);
+void timeval_sub(struct timeval *out, struct timeval *in);
 
 #endif
