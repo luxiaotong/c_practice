893892d48b75fb30b3a32cfd8049c4d6bac236c1
Shannon Lu <luxiaotong713@sina.com>
2022-07-25 11:24:44 +0800

    c++ tutorial - variables & data types

---
+++ cpp_tutorial_for_beginers/data_types.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char grade = 'A';
+    string phrase = "Test Academy";
+    int age = 50;
+    // float
+    double gpa = 2.3;
+    bool isMale = true;
+    cout << grade << endl;
+    return 0;
+}
