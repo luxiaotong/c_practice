cadb22ec6a5f602951b1f1ea46f13cd2d4217486
Shannon Lu <luxiaotong713@sina.com>
2022-07-25 18:31:45 +0800

    c++ tutorial - get user input

---
+++ cpp_tutorial_for_beginers/get_user_input.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    // int age;
+    // cout << "Enter your age: ";
+    // cin >> age;
+    // cout << "You are " << age << " years old" << endl;
+
+    string name;
+    cout << "Enter your name: ";
+    getline(cin, name);
+    cout << "Hello " << name << endl;
+    return 0;
+}
