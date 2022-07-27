3e2e35bba20bdcc80c8ad7c0c554503e850a0d1b
Shannon Lu <luxiaotong713@sina.com>
2022-07-27 14:56:57 +0800

    c++ tutorial - calculator

---
+++ cpp_tutorial_for_beginers/calculator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double num1, num2;
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    cout << num1 + num2 << endl;
+
+    return 0;
+}
